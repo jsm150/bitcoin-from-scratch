e6255e34ad348a1f58b3c3edea7d33b4da7a1b99
Jung Sumin <drt00784@gmail.com>
2025-08-22 22:23:13 +0900

    Feat: 공개키의 SEC 표준 직렬화/역직렬화를 추가한다

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use bitcoin_practice::encrypt::{Secp256k1};
+use bitcoin_practice::encrypt::{Secp256k1, PublicKey};
 use rand::{Rng};
 use ruint::aliases::U256;
 
 
 fn main() {
-    let a = Secp256k1::default();
+    let key = PublicKey::build(Secp256k1::default() * 0xdeadbeef12345).unwrap();
+    println!("{:02X?}", key.to_uncompress_sec());
 }
